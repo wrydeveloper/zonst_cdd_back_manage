6057384ca6f63c473d7bc43ab57dd4f2cfd452e7
wangruyin <13047908816@163.com>
2018-06-29 17:48:15 +0800

    另一个用到的数据库

---
+++ pay.sql
@@ -0,0 +1,26 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 服务器菜多多后台
+ Source Server Type    : PostgreSQL
+ Source Server Version : 90607
+ Source Host           : 193.112.123.58
+ Source Database       : cddpayapp
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 90607
+ File Encoding         : utf-8
+
+ Date: 06/29/2018 17:33:55 PM
+*/
+
+-- ----------------------------
+--  Table structure for cash
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."cash";
+CREATE TABLE "public"."cash" (
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "public"."cash" OWNER TO "cddpay";
+
